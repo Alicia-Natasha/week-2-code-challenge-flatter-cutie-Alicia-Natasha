a7976e1eb14ff65a71da1316e68a0bfa01feb86a
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-24 09:38:02 +0300

    week-2-code-challenge

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
         characters.forEach(character => {
     //         // Create a span tag for each character and set its name
             const characterSpan = document.createElement('span');
-    //         characterSpan.textContent = character.name;
+            characterSpan.textContent = character.name;
     //         characterSpan.setAttribute('data-id', character.id); // Store character id on span for later use
     //            // Append the character name span to the character bar
     //            characterBar.appendChild(characterSpan);
             });
         })
-    //     .catch(error => console.error('Error fetching characters:', error));
+        .catch(error => console.error('Error fetching characters:', error));
 });
 
 // // // Step 2: Show character details when clicked
